955d7b67e8a2f3c5f6a30c926ab06f58986ee769
aoyamadev <106678583+aoyamaartworkdev@users.noreply.github.com>
2025-06-08 01:00:48 +0900

    remove: Root Groups変更による不要になったファイルを削除
    - Aboutページ、Connectionページ、Dashboard関連のスタイルおよびコンポーネントファイルを削除しました。
    - プライバシーポリシーと利用規約のページに関連するスタイルファイルも削除しました。
    - これにより、コードベースをクリーンに保ち、メンテナンス性を向上させました。

--- src/app/(connection)/connection/page.tsx
+++
@@ -1,9 +0,0 @@
-import * as Connection from "@/features/connection/components";
-import { getPageMetadata } from "@/config/metadata";
-import { Metadata } from "next";
-
-export const metadata: Metadata = getPageMetadata("connection");
-
-export default function ConnectionPage() {
-  return <Connection.ConnectionPageClient />;
-}
