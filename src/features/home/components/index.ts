00f1b65fa084aba4862b884bb3611a3dfa086c62
aoyamadev <106678583+aoyamaartworkdev@users.noreply.github.com>
2025-06-09 01:25:32 +0900

    remove: HomeTypingAnimationコンポーネントと関連CSSファイルを削除し、index.tsからのエクスポートを更新

--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -1,13 +1,11 @@
 import HomeAnimatedTitle from "./HomeAnimatedTitle/HomeAnimatedTitle";
 import HomeAnimatedSubTitle from "./HomeAnimatedSubTitle/HomeAnimatedSubTitle";
-import HomeTypingAnimation from "./HomeTypingAnimation/HomeTypingAnimation";
 import HomeStartButton from "./HomeStartButton/HomeStartButton";
 import HomeAnimatedCharacter from "./HomeAnimatedCharacter/HomeAnimatedCharacter";
 
 export {
   HomeAnimatedTitle,
   HomeAnimatedSubTitle,
-  HomeTypingAnimation,
   HomeStartButton,
   HomeAnimatedCharacter,
 };
