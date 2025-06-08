3320c215b93d988d890a514a0b97f8ffd8246eb9
aoyamadev <106678583+aoyamaartworkdev@users.noreply.github.com>
2025-06-09 01:14:13 +0900

    remove: AboutTypingAnimationコンポーネントを削除し、index.tsからのエクスポートを更新

--- src/features/about/components/index.ts
+++ src/features/about/components/index.ts
@@ -1,4 +1,3 @@
-import AboutTypingAnimation from "./AboutTypingAnimation/AboutTypingAnimation";
 import AboutLink from "./AboutLink/AboutLink";
 
-export { AboutTypingAnimation, AboutLink };
+export { AboutLink };
