bc39f59ac44a5082858f4528e6323fd45b450cda
aoyamadev <106678583+aoyamaartworkdev@users.noreply.github.com>
2025-06-09 14:13:41 +0900

    update: ConnectionAuthSectionコンポーネントのスタイルを改善し、テキストを更新。詳細リンクを追加し、全体のレイアウトを調整。

--- src/features/connection/components/ConnectionAuthSection/ConnectionAuthSection.tsx
+++ src/features/connection/components/ConnectionAuthSection/ConnectionAuthSection.tsx
@@ -1,67 +1,83 @@
 import React from "react";
 import styles from "./ConnectionAuthSection.module.css";
 import AuthButton from "@/components/auth/AuthButton/AuthButton";
+import Link from "next/link";
 
 interface ConnectionAuthSectionProps {
-  loading: boolean;
-  zennUsername: string;
-  updateUserProfile: () => void;
+	loading: boolean;
+	zennUsername: string;
+	updateUserProfile: () => void;
 }
 
 const ConnectionAuthSection: React.FC<ConnectionAuthSectionProps> = ({
-  loading,
-  zennUsername,
-  updateUserProfile,
+	loading,
+	zennUsername,
+	updateUserProfile,
 }) => {
-  return (
-    <div className={`px-4 py-6 grid gap-8 ${styles["connection-container"]}`}>
-      <div className={`${styles["auth-content"]}`}>
-        <AuthButton />
-        <p className="text-center">
-          アプリを利用するには「ログイン」または「新規登録」が必要です。
-        </p>
-      </div>
-      <hr className={styles["center-line"]} />
-      <div className={`grid grid-cols-1 gap-2 ${styles["zenn-connect-area"]}`}>
-        <label
-          htmlFor="zenn-username"
-          className="text-sm opacity-40 select-none"
-        >
-          Zennユーザー名
-          <strong className="text-[#ffc630]">（必須）</strong>
-        </label>
-        <div className="flex gap-3 opacity-40 select-none">
-          <input
-            id="zenn-username"
-            type="text"
-            value=""
-            onChange={() => {}}
-            className="flex-1 border-[3px] border-gray-400 bg-white rounded px-3 py-2 text-black cursor-not-allowed"
-            placeholder="例: aoyamadev"
-            disabled
-          />
-          <button
-            onClick={() => updateUserProfile()}
-            className={`${styles["connect-button"]} ${
-              !loading && zennUsername ? styles["active"] : ""
-            } ${
-              loading || !zennUsername
-                ? "opacity-50 cursor-not-allowed"
-                : "cursor-pointer"
-            }`}
-            disabled={loading || !zennUsername}
-          >
-            <div className={`${styles["connect-button-content"]}`}>
-              {loading ? "連携中..." : "連携"}
-            </div>
-          </button>
-        </div>
-        <p className="text-center mt-[12px]">
-          先に「ログイン」または「新規登録」を完了してください。
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={`px-4 pt-3 pb-6 grid gap-8 ${styles["connection-container"]}`}
+		>
+			<div className={`${styles["auth-content"]}`}>
+				<AuthButton />
+				<div className="grid grid-cols-1 gap-[32px]">
+					<p className="text-center text-base">
+						Zennとの連携には「ログイン」または「新規登録」が必要です。
+					</p>
+					<div className="grid grid-cols-1 gap-2 place-items-center">
+						<em className="text-center text-sm not-italic">
+							※「ログイン」「新規登録」無しでも、ご利用いただけます。
+						</em>
+						<Link
+							href="/connection-detail"
+							className={styles["connection-detail-link"]}
+						>
+							詳細はこちら
+						</Link>
+					</div>
+				</div>
+			</div>
+			<hr className={styles["center-line"]} />
+			<div className={`grid grid-cols-1 gap-2 ${styles["zenn-connect-area"]}`}>
+				<label
+					htmlFor="zenn-username"
+					className="text-sm opacity-40 select-none"
+				>
+					Zennユーザー名
+					<strong className="text-[#ffc630]">（必須）</strong>
+				</label>
+				<div className="flex gap-3 opacity-40 select-none">
+					<input
+						id="zenn-username"
+						type="text"
+						value=""
+						onChange={() => {}}
+						className="flex-1 border-[3px] border-gray-400 bg-white rounded px-3 py-2 text-black cursor-not-allowed"
+						placeholder="例: aoyamadev"
+						disabled
+					/>
+					<button
+						onClick={() => updateUserProfile()}
+						className={`${styles["connect-button"]} ${
+							!loading && zennUsername ? styles["active"] : ""
+						} ${
+							loading || !zennUsername
+								? "opacity-50 cursor-not-allowed"
+								: "cursor-pointer"
+						}`}
+						disabled={loading || !zennUsername}
+					>
+						<div className={`${styles["connect-button-content"]}`}>
+							{loading ? "連携中..." : "連携"}
+						</div>
+					</button>
+				</div>
+				<p className="text-center text-base mt-[12px]">
+					先に「ログイン」または「新規登録」を完了してください。
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default ConnectionAuthSection;
