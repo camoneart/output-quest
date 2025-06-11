54b82376f1e91e44812792ab81be03b69ac72d0a
aoyamadev <106678583+aoyamaartworkdev@users.noreply.github.com>
2025-06-12 05:14:37 +0900

    style: 各コンポーネントのCSSにテキストシャドウを追加し、視認性を向上

--- src/features/strength/components/StrengthHeroInfo/StrengthHeroInfo.tsx
+++ src/features/strength/components/StrengthHeroInfo/StrengthHeroInfo.tsx
@@ -79,7 +79,7 @@ const StrengthHeroInfo = () => {
 		return (
 			<div className={styles["strength-hero-info"]}>
 				<div className={styles["strength-hero-info-content"]}>
-					<h2 className={styles["strength-title-title"]}>~ 勇者のレベル ~</h2>
+					<h2 className={styles["strength-hero-info-title"]}>~ 勇者のレベル ~</h2>
 					<div className={styles["error-text"]}>{error}</div>
 				</div>
 			</div>
@@ -89,7 +89,7 @@ const StrengthHeroInfo = () => {
 	return (
 		<div className={styles["strength-hero-info"]}>
 			<div className={styles["strength-hero-info-content"]}>
-				<h2 className={styles["strength-title-title"]}>~ 勇者のレベル ~</h2>
+				<h2 className={styles["strength-hero-info-title"]}>~ 勇者のレベル ~</h2>
 				<div className={styles["strength-hero-info-content-head"]}>
 					{/* 勇者のアイコン　*/}
 					<div className={styles["strength-hero-box"]}>
