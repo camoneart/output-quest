270ee115b91a5d09b27d524880f626f4e705385e
aoyamadev <106678583+aoyamaartworkdev@users.noreply.github.com>
2025-06-13 23:43:49 +0900

    add: OpenGraph用の画像ファイルを追加し、サイト設定のOpenGraph画像URLを更新しました。

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,17 +1,17 @@
 export const siteData = {
-  siteMainTitle: "OUTPUT QUEST",
-  siteSubtitle: "~ 叡智の継承者 ~",
-  siteFullTitle: "OUTPUT QUEST　~ 叡智の継承者 ~",
-  siteDescription:
-    "OUTPUT QUEST　~ 叡智の継承者 ~ は、RPG風のゲーミフィケーションを取り入れた「新感覚学習RPG」です。Zennで記事を書き、勇者を育てる学びの冒険をいま、始めよう！",
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://outputquest.com",
-  author: "aoyama",
-  copyright: "© 2025 OUTPUT QUEST",
+	siteMainTitle: "OUTPUT QUEST",
+	siteSubtitle: "~ 叡智の継承者 ~",
+	siteFullTitle: "OUTPUT QUEST　~ 叡智の継承者 ~",
+	siteDescription:
+		"OUTPUT QUEST　~ 叡智の継承者 ~ は、RPG風のゲーミフィケーションを取り入れた「新感覚学習RPG」です。Zennで記事を書き、勇者を育てる学びの冒険をいま、始めよう！",
+	siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://outputquest.com",
+	author: "aoyama",
+	copyright: "© 2025 OUTPUT QUEST",
 };
 
 export const openGraphImage = {
-  url: "/opengraph-image.png",
-  width: 1200,
-  height: 630,
-  alt: siteData.siteFullTitle,
+	url: "/images/opengraph/opengraph-image.png",
+	width: 1200,
+	height: 630,
+	alt: siteData.siteFullTitle,
 };
