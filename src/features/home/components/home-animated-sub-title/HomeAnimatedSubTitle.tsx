bfaf24457387ed697cd8ae9045ff77440ecace05
aoyamadev <106678583+aoyamaartworkdev@users.noreply.github.com>
2025-06-20 01:34:31 +0900

    style: HomeAnimatedSubTitleから「~」をremoveしてスタイル変更。

--- src/features/home/components/home-animated-sub-title/HomeAnimatedSubTitle.tsx
+++ src/features/home/components/home-animated-sub-title/HomeAnimatedSubTitle.tsx
@@ -5,26 +5,24 @@ import styles from "./HomeAnimatedSubTitle.module.css";
 import { useState, useEffect } from "react";
 
 const HomeAnimatedSubTitle = () => {
-  const [isActive, setIsActive] = useState(false);
+	const [isActive, setIsActive] = useState(false);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsActive(true);
-    }, 8200);
-    return () => clearTimeout(timer);
-  }, []);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setIsActive(true);
+		}, 8200);
+		return () => clearTimeout(timer);
+	}, []);
 
-  return (
-    <h2
-      className={`${styles["subtitle"]} ${
-        isActive ? styles["active-subtitle"] : ""
-      }`}
-    >
-      <span className={styles["subtitle-text"]}>~</span>
-      叡智の継承者
-      <span className={styles["subtitle-text"]}>~</span>
-    </h2>
-  );
+	return (
+		<h2
+			className={`${styles["subtitle"]} ${
+				isActive ? styles["active-subtitle"] : ""
+			}`}
+		>
+			叡智の継承者
+		</h2>
+	);
 };
 
 export default HomeAnimatedSubTitle;
