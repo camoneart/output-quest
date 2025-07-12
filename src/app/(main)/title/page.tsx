87e0eb351eee11ee0f78095a8316a749f4dfd61a
aoyamadev <106678583+bojjisage@users.noreply.github.com>
2025-07-13 07:39:49 +0900

    style: タイトルページに背景を追加し、レスポンシブデザインを改善。1024px未満の画面幅でのオーバーフローを調整。

--- src/app/(main)/title/page.tsx
+++ src/app/(main)/title/page.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = getPageMetadata("title");
 const TitlePage = () => {
 	return (
 		<>
+			<div className={styles["title-bg"]}></div>
 			<h1 className={`${styles["title-page-title"]}`}>称号リスト</h1>
 			<Title.TitlePageClient />
 		</>
