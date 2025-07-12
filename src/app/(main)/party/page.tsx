4a3ed874ef67ac63fd7697fad05efb355e4ca462
aoyamadev <106678583+bojjisage@users.noreply.github.com>
2025-07-13 07:40:38 +0900

    style: パーティページにタイトル背景を追加し、レスポンシブデザインを改善。1024px未満の画面幅でのオーバーフローとパディングを調整。

--- src/app/(main)/party/page.tsx
+++ src/app/(main)/party/page.tsx
@@ -7,22 +7,23 @@ import * as Party from "@/features/party/components/index";
 export const metadata: Metadata = getPageMetadata("party");
 
 const PartyPage = () => {
-  return (
-    <>
-      <h1 className={`${styles["party-title"]}`}>なかま</h1>
-      <div className={`${styles["party-container"]}`}>
-        <div className={`${styles["party-header"]}`}>
-          <p>勇者の仲間を確認できます。</p>
-          <p>仲間になるキャラクターは最大で30人です。</p>
-          <p>仲間をクリックすると、仲間の詳細を確認できます。</p>
-        </div>
+	return (
+		<>
+			<div className={styles["title-bg"]}></div>
+			<h1 className={`${styles["party-title"]}`}>なかま</h1>
+			<div className={`${styles["party-container"]}`}>
+				<div className={`${styles["party-header"]}`}>
+					<p>勇者の仲間を確認できます。</p>
+					<p>仲間になるキャラクターは最大で30人です。</p>
+					<p>仲間をクリックすると、仲間の詳細を確認できます。</p>
+				</div>
 
-        <hr className={styles["party-line"]} />
+				<hr className={styles["party-line"]} />
 
-        <Party.PartyMemberCardList />
-      </div>
-    </>
-  );
+				<Party.PartyMemberCardList />
+			</div>
+		</>
+	);
 };
 
 export default PartyPage;
